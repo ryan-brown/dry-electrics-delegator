0b1014a704620f9bc85c52459e79e7150f29454c
Ryan Brown <brown.ry@husky.neu.edu>
2018-11-08 19:05:28 -0500

    fix low percents

--- bg.sh
+++ bg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 server='https://electrics.fortheusers.org'
-percentage=$(pmset -g batt | grep -Eho '(...)%' | xargs | sed 's/%//')
+percentage=$(pmset -g batt | grep -Eho '([0-9]*)%' | xargs | sed 's/%//')
 
 discharging=$(pmset -g batt | grep 'discharging')
 chargestatus=$([ -z "$discharging" ] && echo 1 || echo 0)
