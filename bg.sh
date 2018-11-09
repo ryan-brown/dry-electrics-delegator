719a713fef8a0aa305864fc62848a5b7b705f125
Chris Ciollaro <cciollaro@localytics.com>
2018-11-08 21:06:18 -0500

    if you pass a name to the script, it uses it. otherwise it uses your $USER

--- bg.sh
+++ bg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+username=${1:-$USER}
 server='https://electrics.fortheusers.org'
 percentage=$(pmset -g batt | grep -Eho '([0-9]*)%' | xargs | sed 's/%//')
 
 discharging=$(pmset -g batt | grep 'discharging')
 chargestatus=$([ -z "$discharging" ] && echo 1 || echo 0)
 
-curl --http1.1 --header "Content-Type: application/json" --data "{\"username\":\"$USER\",\"percentage\":$percentage,\"charging\":$chargestatus}" $server/percentage
+curl --http1.1 --header "Content-Type: application/json" --data "{\"username\":\"$username\",\"percentage\":$percentage,\"charging\":$chargestatus}" $server/percentage
