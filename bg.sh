f584f92140b459ae6e13e322ab1facd02cfeea5e
Jonathan McKenzie <jmckenzie@localytics.com>
2018-11-09 10:45:48 -0500

    Suppress all output except for errors to reduce mail volume

--- bg.sh
+++ bg.sh
@@ -6,4 +6,4 @@ percentage=$(pmset -g batt | grep -Eho '([0-9]*)%' | xargs | sed 's/%//')
 discharging=$(pmset -g batt | grep 'discharging')
 chargestatus=$([ -z "$discharging" ] && echo 1 || echo 0)
 
-curl --http1.1 --header "Content-Type: application/json" --data "{\"username\":\"$username\",\"percentage\":$percentage,\"charging\":$chargestatus}" $server/percentage
+curl --http1.1 --header "Content-Type: application/json" --data "{\"username\":\"$username\",\"percentage\":$percentage,\"charging\":$chargestatus}" $server/percentage --fail --silent --show-error >/dev/null
