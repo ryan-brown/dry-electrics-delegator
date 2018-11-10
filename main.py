5fe83927aa9f6f3d933cea77554ecec89215b033
Ryan Brown <brown.ry@husky.neu.edu>
2018-11-09 19:15:37 -0500

    dont need these imports

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from flask import Flask, request, send_from_directory
-import maya
-import cgi
-import datetime
-import os
-import database
-
 from app import create_app
+
 app = create_app()
 
 if __name__ == '__main__':
