42ed845723a35d09a97618065bf4a799796b6e5f
Jonathan McKenzie <jmckenzie@localytics.com>
2019-04-19 19:38:18 -0400

    Remove print, check for none in queue

--- qq.py
+++ qq.py
@@ -6,31 +6,22 @@ import redis
 redis_db = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
 
 def insert_update(username, percentage, charging, updated_at):
-    redis_db.rpush('write_queue', json.dumps({
-        'username': username,
-        'percentage': percentage,
-        'charging': charging,
-        'updated_at': updated_at.isoformat()
-        }));
     update_data = json.dumps({
         'username': username,
         'percentage': percentage,
         'charging': charging,
         'updated_at': updated_at.isoformat()
         }); 
-    print("update_data", update_data)
 
-    redis_db.set('user_%s' % username, json.dumps({
-        'username': username,
-        'percentage': percentage,
-        'charging': charging,
-        'updated_at': updated_at.isoformat()
-        }));
+    redis_db.rpush('write_queue', update_data)
+    redis_db.set('user_%s' % username, update_data)
 
 def read_from_queue(max_count=1000):
     batch = []
     for count in range(max_count):
-        batch.append(json.loads(redis_db.lpop('write_queue')))
+        update = redis_db.lpop('write_queue')
+        if update is not None:
+            batch.append(json.loads(update))
     return batch
 
 def get_user_data(username):
