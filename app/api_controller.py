9c25dc775afc4a1022bd3b2a4f57e70806a80472
Jonathan McKenzie <jmckenzie@localytics.com>
2019-04-22 10:53:33 -0400

    Don't print duh

--- app/api_controller.py
+++ app/api_controller.py
@@ -30,7 +30,6 @@ def get_leaderboard():
     all_users = qq.get_all_users()
     user_data = { name: qq.get_user_data(str(name)) for name in all_users }
     user_data_items = user_data.items()
-    print(user_data_items)
     sorted_user_data = [item[1] for item in sorted(user_data_items, key=lambda kv: kv[1]['percentage'])]
     formatted_user_data = [(get_row_color(d['percentage']), d['username'], d['percentage'], "🔌 " if d['charging'] else  "🔋 ", maya.parse(d['updated_at'], timezone='US/Eastern')) for d in sorted_user_data]
     return [(d[0], d[1], d[2], d[3], d[4].epoch) for d in formatted_user_data if (maya.now() - d[4]).total_seconds() < 60*10]
