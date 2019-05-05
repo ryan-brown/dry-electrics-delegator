91b8ba25b2cee332f6f0d077f181e4fae8d369b6
ryan-brown <brown.ry@husky.neu.edu>
2019-05-04 22:10:51 -0400

    fix bg.sh

--- bg.sh
+++ bg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 zaptoken=${1:-$ZAPTOKEN}
-server='http://0.0.0.0:5002'
+server='https://electrics.fortheusers.org'
 percentage=$(pmset -g batt | grep -Eho '([0-9]*)%' | xargs | sed 's/%//')
 
 discharging=$(pmset -g batt | grep 'discharging')
 chargestatus=$([ -z "$discharging" ] && echo 1 || echo 0)
 
-curl --http1.1 --header "Content-Type: application/json" --data "{\"zap_token\":\"$zaptoken\",\"percentage\":$percentage,\"charging\":$chargestatus}" $server/percentage
+curl --http1.1 --header "Content-Type: application/json" --data "{\"zap_token\":\"$zaptoken\",\"percentage\":$percentage,\"charging\":$chargestatus}" $server/percentage --fail --silent --show-error >/dev/null
