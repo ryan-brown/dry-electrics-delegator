8d26ad933dbdb093d5f9f5f1fe0e47dd46e7bae6
ryan-brown <brown.ry@husky.neu.edu>
2019-05-06 17:29:02 -0400

    settings updates

--- app/home_controller.py
+++ app/home_controller.py
@@ -1,6 +1,8 @@
-from flask import render_template, Blueprint
-from flask_login import login_required
+from flask import render_template, Blueprint, redirect, url_for, request, flash
+from flask_login import login_required, current_user
+from werkzeug.security import generate_password_hash
 from qq import get_shitposts
+from .models import User, DBSession
 
 home = Blueprint('home', __name__)
 
@@ -17,6 +19,33 @@ def homepage():
 def show_settings():
   return render_template("settings.html")
 
+@home.route("/settings", methods=['POST'])
+@login_required
+def save_settings():
+  email = request.form.get('email')
+  username = request.form.get('username')
+  password = request.form.get('password')
+
+  with DBSession() as session:
+    email_exists = session.query(User).filter(User.id != current_user.id).filter(User.email == email).first()
+    username_exists = session.query(User).filter(User.id != current_user.id).filter(User.username == username).first()
+
+    if email_exists or username_exists:
+      if email_exists:
+        flash('Email already in use.')
+      if username_exists:
+        flash('Username already in use.')
+      return redirect(url_for('home.show_settings'))
+
+    user = session.query(User).filter(User.id == current_user.id).first()
+    user.email = email
+    user.username = username
+    if password:
+      user.password = generate_password_hash(password, method='sha256')
+    session.commit()
+
+  flash('Setting saved!')
+  return redirect(url_for('home.show_settings'))
 
 @home.route("/privacy-policy")
 def policy():
