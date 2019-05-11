38481592c15094ccfb65478588f057ce13c3451f
Ricky Ayoub <rayoub@localytics.com>
2019-05-11 03:27:28 -0400

    restart gunicorn process to reload in prod

--- app/base_controller.py
+++ app/base_controller.py
@@ -15,6 +15,7 @@ def favicon():
 def update():
   try:
     os.system("git pull origin master")
+    os.system(f"kill -HUP {os.getpid()}")
     return "Done", 200
   except Exception as e:
     print(e)
