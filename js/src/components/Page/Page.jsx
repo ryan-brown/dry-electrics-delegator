fd3d51684ed1a67c1ab01b78b22924918b1dc10d
Chris Rollock <Chris@localytics.com>
2019-05-22 23:52:56 -0400

    Adding a splash of electric blue to the pallete (and a bit of space) (#38)

--- js/src/components/Page/Page.jsx
+++ js/src/components/Page/Page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MainHeader from "../MainHeader/MainHeader";
+import MainBody from "../MainBody/MainBody";
 import MainFooter from "../MainFooter/MainFooter";
 import "./Page.css";
 
@@ -7,7 +8,7 @@ const Page = props => (
   <div className="Page">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <MainHeader />
-    <div style={{ flexGrow: "1" }}>{props.children}</div>
+    <MainBody>{props.children}</MainBody>
     <MainFooter />
   </div>
 );
